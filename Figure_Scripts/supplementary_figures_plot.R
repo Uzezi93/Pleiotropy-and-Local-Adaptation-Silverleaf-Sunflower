f4f83e562c6d015a1a7a88245c1bce77d7c14097
uzezi_okinedo001_umb_edu <Uzezi.Okinedo001@umb.edu>
2024-12-15 04:42:58 +0000

    updated r script for plotting supplentary figures

--- Figure_Scripts/supplementary_figures_plot.R
+++ Figure_Scripts/supplementary_figures_plot.R
@@ -1,27 +1,8 @@
 setwd("../argo_plots/")
 
-# Plot supplementary figures
-# 1. Relationship between outlier pvalues and connectivity
-# write.table(argo.pv, file = "./argo.pv.txt", sep = "\t", quote = FALSE)
-# write.table(argo.pv2, file = "argo.pv2.txt", sep = "\t", quote = FALSE)
-# write.table(argo.pv3, file = "argo.pv3.txt", sep = "\t", quote = FALSE)
-# write.table(argo.pv4, file = "argo.pv4.txt", sep = "\t", quote = FALSE)
-# write.table(argo.pv5, file = "argo.pv5.txt", sep = "\t", quote = FALSE)
-# write.table(res, file = "pcadapt_res.txt", sep = "\t", quote = FALSE)
-
-
-#LFMM_pvalue <- data.frame(cbind(argo.pv$pvalue, argo.pv2$pvalue, argo.pv3$pvalue, argo.pv4$pvalue, argo.pv5$pvalue)) %>%
- # na.omit()
-#LFMM_pvalue <- gather(LFMM_pvalue)
-#LFMM_pvalue <- LFMM_pvalue$value
-#PCAdapt_pvalue <- res$pvalues
-
-# Save to pvalues
-#cat(PCAdapt_pvalue, file = "PCAdapt_pvalues.txt")
-#cat(LFMM_pvalue, file = "LFMM_pvalues.txt")
-
+# Plot the relationship between lfmm and pcadapt gene connectivity and pvalues from both analysis
 # Get connectivity values
-# Change directory
+
 LFMM_pvalue <- read.table("../network_analysis/lfmm_pvalues.txt", header = F)
 names(LFMM_pvalue) <- c("pvalue", "gene")
 PCAdapt_pvalue <- read.table("../network_analysis/pcadapt_pvalues.txt", header = F)
@@ -93,19 +74,6 @@ lm_connectivity <- plot_grid(lfmm_lm_connectivity, pcadapt_lm_connectivity)
 ggsave("PLOS_genetics_figures/pval_lm_connectivity.tiff", plot = lm_connectivity, device = "tiff", width = 10, height = 5)
 
 
-# density plot of connectivity values
-lfmm_connectivity_density <- ggplot(data, aes(x = lfmm_connectivity)) +
-  geom_density() +
-  labs(x = "Connectivity", y = "LFMM p-values")
-
-pcadapt_connectivity_density <- ggplot(data2, aes(x = pcadapt_connectivity)) +
-  geom_density() +
-  labs(x = "Connectivity", y = "PCAdapt p-values")
-
-density_connectivity <- plot_grid(lfmm_connectivity_density , pcadapt_connectivity_density)
-ggsave("PLOS_genetics_figures/connectivity_density.tiff", plot = density_connectivity, device = "tiff", width = 10, height = 5)
-
-
 # Plot pairwise fst
 fst <- read.table(file="../../angsd/FST/fst_stat",sep="\t",header=F);
 colnames(fst) <- c("chromosomes", "positions", "unweighted_FST", "weighted_FST")
@@ -134,36 +102,6 @@ plot(fst$midp, fst$fst, xlab="Physical position", ylab="Fst", col="#5f9ea0", pch
 
 
 
-
-
-
-
-# Do angsd for north and coast populations to get pestg files
-north_pop_theta <- read.table(file = "../../angsd/SFS2/north_pop.thetas.idx.pestPG", sep = "\t", header = F)
-
-coast_pop_theta <- read.table(file = "../../angsd/SFS2/coast_pop.thetas.idx.pestPG", sep = "\t", header = F)
-
-# Rename columns
-colnames(north_pop_theta) <- c("(indexStart,indexStop)", "chrname", "wincenter", "tW", "tP", "tF", "tH", "tL", "tajD", "fulif", "fuliD", "fayH", "zengsE", "numSites")
-colnames(coast_pop_theta) <- c("(indexStart,indexStop)", "chrname", "wincenter", "tW", "tP", "tF", "tH", "tL", "tajD", "fulif", "fuliD", "fayH", "zengsE", "numSites")
-
-north_pop <- north_pop_theta %>%
-  dplyr::select(., tW, numSites, tP, tajD) %>%
-  mutate(Watterson_theta = tW/numSites) %>%
-  mutate(Theta_pie = tP/numSites) %>%
-  na.omit()
-tajD <- mean(north_pop$tajD) # -0.1745136
-Watterson_theta <- mean(north_pop$Watterson_theta) # 0.2076168
-Theta_pie <- mean(north_pop$Theta_pie) # 0.1937444
-
-coast_pop <- coast_pop_theta %>%
-  dplyr::select(., tW, numSites, tP, tajD) %>%
-  mutate(Watterson_theta = tW/numSites) %>%
-  mutate(Theta_pie = tP/numSites) %>%
-  na.omit()
-tajD <- mean(coast_pop$tajD) # -0.2985429
-Watterson_theta <- mean(coast_pop$Watterson_theta) # 0.2353111
-Theta_pie <- mean(coast_pop$Theta_pie) # 0.2060006
 
 
 
